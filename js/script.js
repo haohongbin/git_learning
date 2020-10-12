0e55a716b04f8eda82320ca951c16f210d43b226
hhb <630936210@qq.com>
2020-10-12 17:13:23 +0800

    修改script.js

--- js/script.js
+++ js/script.js
@@ -7,6 +7,7 @@ for (i = 0; i < acc.length; i++) {
         if (panel.style.display === "block") {
             panel.style.display = "none";
         } else {
+
             panel.style.display = "block";
         }
     }
